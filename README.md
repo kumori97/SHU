8cb086afa66c7a6598cec334d21a3d305443f176
kumori <844347175@qq.com>
2021-01-27 21:41:13 +0800

    qqq

---
+++ README.md
@@ -0,0 +1,108 @@
+# 上海大学健康之路每日一报/每日两报自动打卡
+
+程序为python脚本文件，修改配置文件相关信息，设置后台运行脚本，脚本会根据配置文件信息自动进行每日一报/每日两报。
+
+支持github actions服务器。
+
+支持一键补报功能。
+
+支持每日一报/每日两报.
+
+目前暂时同时进行每日一报和每日两报，没有设置可编辑参数项，地址为随机地址。
+
+## 免责申明
+
+本项目仅做免费的学术交流使用。
+
+## 用法
+
+### 0. ⭐Star⭐项目😉
+
+如果你想获取最新的更新，记得右上角的`watch`
+
+### 1. 你有服务器，只在自己服务器上进行自动打卡
+
+在 `config.yaml` 中设置所有需要打卡的学号密码
+
+**本程序自带一键补报功能**，如需补报，定位到 `main.py` 第14行
+
+```python
+NEED_BEFORE = False  # 如需补报则置为True，否则False
+START_DT = dt.datetime(2020, 11, 10)  # 需要补报的起始日期
+```
+
+针对Ubuntu，编辑定时执行程序cron
+
+```bash
+crontab -e
+```
+
+加入以下命令：
+
+```bash
+# 程序每天8点与20点执行一次，并将结果输出至shu_report.log
+# 注意python的路径，main.py与输出日志shu_report.log的绝对路径
+0 8,20 * * * python -u /xxx/main.py 2>&1 >> /xxx/shu_report.log
+```
+
+如果你服务器是UTC时区，则为
+
+```bash
+0 11,23 * * * python -u /xxx/main.py 2>&1 >> /xxx/shu_report.log
+```
+
+如果你不确定你服务器的时区，也可以每小时运行一次：
+
+```bash
+0 * * * * python -u /xxx/main.py 2>&1 >> /xxx/shu_report.log
+```
+
+### 2. 你没有服务器，使用 github actions（推荐）
+
+**在该模式下请不要开启补报功能**
+
+#### 你第一次Fork
+
+1. fork 该仓库至你的仓库下
+2. 定位到你的仓库的`Settings`的`Secrets`选项卡
+3. 添加secret（New repository secret）
+
+`NAME` 设置为 `USERS`
+
+`VALUE` 设置为 `学号1,密码1;学号2,密码2` 的格式，注意逗号与分号的区分，学号密码之间用逗号，每两个学号之间用分号，必须是英文半角符号，如果只有一个学号密码则不需要加分号
+
+![](images/secrets.png)
+
+4. 定位到你仓库下的 `Actions` 选项卡，点击 `Enable workflow`
+
+![](images/enable_actions.png)
+
+5. 此时Actions 已经启动完成，每天上午和下午七点（UTC+8）各执行一次，每执行一次会在 `Actions` 选项卡下生成一个报告。
+
+   如果需要对报送功能进行测试，可以点击 `run workflow` 按钮，立即进行一次运行。
+
+   ![](images/run_workflow.png)
+
+![](images/actions.png)
+
+
+#### 你第二次Fork，要更新原先内容
+
+![](images/update_fork.gif)
+
+或者最简单的方法， 删除你fork的项目，重新fork
+
+
+
+## 依赖
+
+- python3
+- 依赖：
+  - pyyaml
+  - beautifulsoup4
+  - requests
+
+## 感谢
+
+感谢各位 contributors 所做的贡献。
+
---
+++ main.py
@@ -0,0 +1,256 @@
+import datetime as dt
+import os
+import random
+import time
+from pathlib import Path
+
+import requests
+import yaml
+from bs4 import BeautifulSoup
+
+from fstate_generator import generate_fstate_day, generate_fstate_halfday
+from login import login
+
+NEED_BEFORE = False  # 如需补报则置为True，否则False
+START_DT = dt.datetime(2020, 1, 26)  # 需要补报的起始日期
+
+
+# 获取东八区时间
+def get_time():
+    # 获取0时区时间，变换为东八区时间
+    # 原因：运行程序的服务器所处时区不确定
+    t = dt.datetime.utcnow()
+    t = t + dt.timedelta(hours=8)
+
+    # 或者：
+    # t = dt.datetime.utcnow()
+    # tz_utc_8 = dt.timezone(dt.timedelta(hours=8))
+    # t = t.astimezone(tz_utc_8)
+
+    # 如果服务器位于东八区，也可用：
+    # t = dt.datetime.now()
+
+    return t
+
+
+# 随机生成地址
+def get_random_address():
+    address = [chr(random.randint(0x4e00, 0x9fbf)) for _ in range(3)]
+    address = ''.join(address) + '路'
+    address += str(random.randint(1, 999)) + '号'
+    address += str(random.randint(1, 20)) + '0' + str(random.randint(1, 9)) + '室'
+    return address
+
+
+def report_day(sess, t):
+    url = f'https://selfreport.shu.edu.cn/DayReport.aspx?day={t.year}-{t.month}-{t.day}'
+
+    r = sess.get(url)
+    soup = BeautifulSoup(r.text, 'html.parser')
+    view_state = soup.find('input', attrs={'name': '__VIEWSTATE'})
+
+    if view_state is None:
+        if '上海大学统一身份认证' in r.text:
+            print('登录信息过期')
+        else:
+            print(r.text)
+        return False
+
+    BaoSRQ = t.strftime('%Y-%m-%d')
+    XiangXDZ = get_random_address()
+
+    while True:
+        try:
+            r = sess.post(url, data={
+                "__EVENTTARGET": "p1$ctl00$btnSubmit",
+                "__EVENTARGUMENT": "",
+                "__VIEWSTATE": view_state['value'],
+                "__VIEWSTATEGENERATOR": "7AD7E509",
+                "p1$ChengNuo": "p1_ChengNuo",
+                "p1$BaoSRQ": BaoSRQ,
+                "p1$DangQSTZK": "良好",
+                "p1$TiWen": "",
+                "p1$JiuYe_ShouJHM": "",
+                "p1$JiuYe_Email": "",
+                "p1$JiuYe_Wechat": "",
+                "p1$QiuZZT": "",
+                "p1$JiuYKN": "",
+                "p1$JiuYSJ": "",
+                "p1$ZaiXiao": "不在校",
+                "p1$MingTDX": "不到校",
+                "p1$MingTJC": "否",
+                "p1$BanChe_1$Value": "0",
+                "p1$BanChe_1": "不需要乘班车",
+                "p1$BanChe_2$Value": "0",
+                "p1$BanChe_2": "不需要乘班车",
+                "p1$GuoNei": "国内",
+                "p1$ddlGuoJia$Value": "-1",
+                "p1$ddlGuoJia": "选择国家",
+                "p1$ShiFSH": "是",
+                "p1$ShiFZX": "否",
+                "p1$ddlSheng$Value": "上海",
+                "p1$ddlSheng": "上海",
+                "p1$ddlShi$Value": "上海市",
+                "p1$ddlShi": "上海市",
+                "p1$ddlXian$Value": "宝山区",
+                "p1$ddlXian": "宝山区",
+                "p1$XiangXDZ": XiangXDZ,
+                "p1$FengXDQDL": "否",
+                "p1$TongZWDLH": "否",
+                "p1$CengFWH": "否",
+                "p1$CengFWH_RiQi": "",
+                "p1$CengFWH_BeiZhu": "",
+                "p1$JieChu": "否",
+                "p1$JieChu_RiQi": "",
+                "p1$JieChu_BeiZhu": "",
+                "p1$TuJWH": "否",
+                "p1$TuJWH_RiQi": "",
+                "p1$TuJWH_BeiZhu": "",
+                "p1$QueZHZJC$Value": "否",
+                "p1$QueZHZJC": "否",
+                "p1$DangRGL": "否",
+                "p1$GeLDZ": "",
+                "p1$FanXRQ": "",
+                "p1$WeiFHYY": "",
+                "p1$ShangHJZD": "",
+                "p1$DaoXQLYGJ": "没有",
+                "p1$DaoXQLYCS": "没有",
+                "p1$JiaRen_BeiZhu": "",
+                "p1$SuiSM": "绿色",
+                "p1$LvMa14Days": "是",
+                "p1$Address2": "",
+                "p1_ContentPanel1_Collapsed": "true",
+                "p1_GeLSM_Collapsed": "false",
+                "p1_Collapsed": "false",
+                "F_STATE": generate_fstate_day(BaoSRQ, XiangXDZ)
+            }, headers={
+                'X-Requested-With': 'XMLHttpRequest',
+                'X-FineUI-Ajax': 'true'
+            }, allow_redirects=False)
+        except Exception as e:
+            print(e)
+            continue
+        break
+
+    if any(i in r.text for i in ['提交成功', '历史信息不能修改', '现在还没到晚报时间', '只能填报当天或补填以前的信息']):
+        print(f'{t} 每日一报提交成功')
+        return True
+    else:
+        print(f'{t} 每日一报提交失败')
+        print(r.text)
+        return False
+
+
+def report_halfday(sess, t, temperature=37):
+    ii = '1' if t.hour < 19 else '2'
+
+    url = f'https://selfreport.shu.edu.cn/XueSFX/HalfdayReport.aspx?day={t.year}-{t.month}-{t.day}&t={ii}'
+
+    r = sess.get(url)
+    soup = BeautifulSoup(r.text, 'html.parser')
+    view_state = soup.find('input', attrs={'name': '__VIEWSTATE'})
+
+    if view_state is None:
+        if '上海大学统一身份认证' in r.text:
+            print('登录信息过期')
+        else:
+            print(r.text)
+        return False
+
+    BaoSRQ = t.strftime('%Y-%m-%d')
+
+    while True:
+        try:
+            r = sess.post(url, data={
+                "__EVENTTARGET": "p1$ctl00$btnSubmit",
+                "__EVENTARGUMENT": "",
+                "__VIEWSTATE": "",
+                "__VIEWSTATEGENERATOR": "DC4D08A3",
+                "p1$ChengNuo": "p1_ChengNuo",
+                "p1$BaoSRQ": BaoSRQ,
+                "p1$DangQSTZK": "良好",
+                "p1$TiWen": str(temperature),
+                "p1$ZaiXiao": "宝山",
+                "p1$ddlSheng$Value": "上海",
+                "p1$ddlSheng": "上海",
+                "p1$ddlShi$Value": "上海市",
+                "p1$ddlShi": "上海市",
+                "p1$ddlXian$Value": "宝山区",
+                "p1$ddlXian": "宝山区",
+                "p1$XiangXDZ": "上海大学",
+                "p1$FengXDQDL": "否",
+                "p1$TongZWDLH": "否",
+                "p1$CengFWH": "否",
+                "p1$CengFWH_RiQi": "",
+                "p1$CengFWH_BeiZhu": "",
+                "p1$JieChu": "否",
+                "p1$JieChu_RiQi": "",
+                "p1$JieChu_BeiZhu": "",
+                "p1$TuJWH": "否",
+                "p1$TuJWH_RiQi": "",
+                "p1$TuJWH_BeiZhu": "",
+                "p1$QueZHZJC$Value": "否",
+                "p1$QueZHZJC": "否",
+                "p1$DangRGL": "否",
+                "p1$GeLDZ": "",
+                "p1$JiaRen_BeiZhu": "",
+                "p1$SuiSM": "绿色",
+                "p1$LvMa14Days": "是",
+                "p1$Address2": "",
+                "F_TARGET": "p1_ctl00_btnSubmit",
+                "p1_ContentPanel1_Collapsed": "true",
+                "p1_GeLSM_Collapsed": "false",
+                "p1_Collapsed": "false",
+                'F_STATE': generate_fstate_halfday(BaoSRQ),
+            }, headers={
+                'X-Requested-With': 'XMLHttpRequest',
+                'X-FineUI-Ajax': 'true'
+            }, allow_redirects=False)
+        except Exception as e:
+            print(e)
+            continue
+        break
+
+    if any(i in r.text for i in ['提交成功', '历史信息不能修改', '现在还没到晚报时间', '只能填报当天或补填以前的信息']):
+        print(f'{t} 每日两报提交成功')
+        return True
+    else:
+        print(f'{t} 每日两报提交失败')
+        print(r.text)
+        return False
+
+
+if __name__ == "__main__":
+    with open(Path(__file__).resolve().parent.joinpath('config.yaml'), encoding='utf8') as f:
+        config = yaml.load(f, Loader=yaml.FullLoader)
+
+    if 'users' in os.environ:
+        for user_password in os.environ['users'].split(';'):
+            user, password = user_password.split(',')
+            config[user] = {
+                'pwd': password
+            }
+
+    for user in config:
+        if user in ['00000000', '11111111']:
+            continue
+
+        print(f'======{user}======')
+        sess = login(user, config[user]['pwd'])
+
+        if sess:
+            now = get_time()
+
+            if NEED_BEFORE:
+                t = START_DT
+                while t < now:
+                    report_day(sess, t)
+                    report_halfday(sess, t + dt.timedelta(hours=8))
+                    report_halfday(sess, t + dt.timedelta(hours=20))
+
+                    t = t + dt.timedelta(days=1)
+
+            report_day(sess, get_time())
+            report_halfday(sess, get_time())
+
+        time.sleep(60)
